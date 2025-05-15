009e197a289d7d45adebaabde8dd486d84e80eb2
tomaton-not <engineer.tomatonton@gmail.com>
2025-05-16 02:59:54 +0900

    画像表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,6 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-        
     if @message.save
       redirect_to room_messages_path(@room)
     else
@@ -18,6 +17,7 @@ class MessagesController < ApplicationController
   end
 
   private
+  
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
